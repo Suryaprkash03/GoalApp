7c190fa6381c00d9c77f502de8925b6c3cb6203f
Suryaprkash03 <suryatheking5784@gmail.com>
2024-01-18 19:35:30 +0530

    Final

--- App.js
+++ App.js
@@ -4,62 +4,59 @@ import GoalInput from './component/GoalInput';
 import GoalItem from './component/GoalItem';
 
 export default function App() {
-  const [modalIsVisible,setModalIsVisible]=useState(false)
-  const [courseGoals,setCourseGoals]=useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false)
+  const [courseGoals, setCourseGoals] = useState([]);
 
   function addGoalHandler(enteredGoaltext) {
-    setCourseGoals(currentCourseGoals =>[...currentCourseGoals,
-      {text:enteredGoaltext, id:Math.random().toString() },]);
-      setEndGoalHandler();
+    setCourseGoals(currentCourseGoals => [...currentCourseGoals,
+    { text: enteredGoaltext, id: Math.random().toString() },]);
+    setEndGoalHandler();
   }
-  function deleteGoaltext (id)
-  {
-    setCourseGoals((currentCourseGoals) =>{
-      return currentCourseGoals.filter((goal)=>goal.id !==id);
+  function deleteGoaltext(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
     });
   }
-  function setAddGoalHandler ()
-  {
+  function setAddGoalHandler() {
     setModalIsVisible(true);
   }
-  function setEndGoalHandler ()
-  {
+  function setEndGoalHandler() {
     setModalIsVisible(false);
   }
   return (
     <View style={styles.appContainer}>
-      <Button title='Add New Goal' color='green' onPress={setAddGoalHandler}/>
-        <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={setEndGoalHandler}/>
+      <Button title='Add New Goal' color='green' onPress={setAddGoalHandler} />
+      <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} onCancel={setEndGoalHandler} />
       <FlatList
         data={courseGoals}
         renderItem={itemData => {
           return (
             <GoalItem text={itemData.item.text}
-            id={itemData.item.id}
-            ondeleteGoal={deleteGoaltext}/>
+              id={itemData.item.id}
+              ondeleteGoal={deleteGoaltext} />
           );
         }}
-        keyExtractor={(item,index)=>{
+        keyExtractor={(item, index) => {
           return item.id;
         }}
-        />
-          
+      />
+
       {/* /*- ************* piecse of Code ************ -*/
-          
-      //     <ScrollView  style={styles.TextInput}>
-      //   {courseGoals.map((goal)=><Text key={goal} style={styles.space}>{goal}</Text>)}
-      // </ScrollView>
-      
+
+        //     <ScrollView  style={styles.TextInput}>
+        //   {courseGoals.map((goal)=><Text key={goal} style={styles.space}>{goal}</Text>)}
+        // </ScrollView>
+
       }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer:{
-    padding:30,
-    marginTop:50,
-    
+  appContainer: {
+    padding: 30,
+    marginTop: 50,
+
   },
 
 
--- component/GoalItem.js
+++ component/GoalItem.js
@@ -1,11 +1,10 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
-function GoalItem (props)
-{
-    return(
+function GoalItem(props) {
+    return (
         <View style={styles.TextInput}>
-            <Pressable android_ripple={{color:'gray'}} onPress={props.ondeleteGoal.bind(this, props.id)}
-            style={({pressed}) => pressed && styles.pressedItem}>
-            <Text style={styles.space}>{props.text}</Text>
+            <Pressable android_ripple={{ color: 'gray' }} onPress={props.ondeleteGoal.bind(this, props.id)}
+                style={({ pressed }) => pressed && styles.pressedItem}>
+                <Text style={styles.space}>{props.text}</Text>
             </Pressable>
         </View>
     );
@@ -14,16 +13,16 @@ export default GoalItem;
 
 const styles = StyleSheet.create({
 
-    TextInput:{
-        margin:8,
-        borderRadius:6,
-        backgroundColor:'black'
+    TextInput: {
+        margin: 8,
+        borderRadius: 6,
+        backgroundColor: 'black'
     },
-    space:{
-        color:'white',
-        padding:12,
+    space: {
+        color: 'white',
+        padding: 12,
     },
-    pressedItem:{
-        opacity:0.5,
+    pressedItem: {
+        opacity: 0.5,
     },
 });
